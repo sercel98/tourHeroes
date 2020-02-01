b0c2b18ab8638d43b34454f131708bbd83fb73e0
Sergio Celemin <sergiocelemin1998@gmail.com>
2020-02-01 11:27:18 -0500

    Se hizo observable la lista de heroes

--- tour-heroes/src/app/heroes/heroes.component.ts
+++ tour-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,19 +9,27 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   currentHero: Hero; 
 
   pastHero: Hero;
 
-  constructor() { }
+  constructor( private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes(); 
   }
 
   onSelect(hero) {
     this.pastHero = this.currentHero;
     this.currentHero = hero;
+  }
+
+  //why void?
+  getHeroes():void {
+
+    this.heroService.getHeroes().subscribe( heroes => this.heroes = heroes);
+  
   }
 
 
