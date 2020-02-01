fd1198e712a907fda4aaa0a117bb42ccc353778d
Sergio Celemin <sergiocelemin1998@gmail.com>
2020-01-31 23:36:48 -0500

    listando heroes

--- tour-heroes/src/app/heroes/heroes.component.ts
+++ tour-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'; 
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,20 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id:1, 
-    name:'Sergio'
-  };  
+  heroes = HEROES;
+  currentHero: Hero; 
+
+  pastHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSelect(hero) {
+    this.pastHero = this.currentHero;
+    this.currentHero = hero;
+  }
+
 
 }
